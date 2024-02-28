d00e0e0dfdfc73c26bc8ffa460ce38350cfa7f05
Jamire44 <2003doyle@gmail.com>
2024-02-28 19:35:09 +0000

    Added BCrypt Security along witha  new Auth Provider

---
+++ src/main/java/com/jdoysu/jdoysu20/config/JdoysuEmailPwdProvider.java
@@ -0,0 +1,55 @@
+package com.jdoysu.jdoysu20.config;
+
+import com.jdoysu.jdoysu20.model.Customer;
+import com.jdoysu.jdoysu20.repository.CustomerRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JdoysuEmailPwdProvider implements AuthenticationProvider {
+
+    private final CustomerRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public JdoysuEmailPwdProvider(CustomerRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customers = repository.findByEmail(username);
+        if (customers.size() > 0){
+            if (passwordEncoder.matches(pwd, customers.get(0).getPwd())){
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd,authorities);
+            } else {
+                throw new BadCredentialsException("Invalid Password");
+            }
+        }else {
+            throw new BadCredentialsException("No User With Them Credentials");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
--- src/main/java/com/jdoysu/jdoysu20/controller/LoginController.java
+++ src/main/java/com/jdoysu/jdoysu20/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.jdoysu.jdoysu20.model.Customer;
 import com.jdoysu.jdoysu20.repository.CustomerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final CustomerRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public LoginController(CustomerRepository repository) {
+    public LoginController(CustomerRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -22,6 +25,8 @@ public class LoginController {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+            String hashedPassword = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashedPassword);
             savedCustomer = repository.save(customer);
             if (savedCustomer.getId() > 0){
                 response = ResponseEntity
